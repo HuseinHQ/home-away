991910c57db6615acf5e1c0ae22e2e1fdf3abe33
HuseinHQ <mhusein463@gmail.com>
2024-10-16 16:14:19 +0700

    Add shadcn components and other components

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,15 @@
+import { Button } from '@/components/ui/button';
 import React from 'react';
 
 function page() {
-  return <h1 className="text-3xl">Home Page</h1>;
+  return (
+    <div>
+      <h1 className="text-3xl">Home Page</h1>
+      <Button variant="outline" size="lg" className="capitalize m-8">
+        Click me
+      </Button>
+    </div>
+  );
 }
 
 export default page;
